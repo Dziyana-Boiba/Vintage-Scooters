40dc414cf716b9962b5de571ef689c952154fa49
Dziyana-Boiba <dziyana.boiba@gmail.com>
2022-08-04 21:25:28 +0100

    first commit
    - Add header, background img, logo, menu

---
+++ js/script.js
@@ -0,0 +1,27 @@
+/*function ibg(){
+    let ibg = document.querySelectorAll('.ibg');
+    for(let i=0; i<ibg.length; i++) {
+        if(ibg[i].querySelector('img')){
+            ibg[i].style.backgroundImage = 'url('+ibg[i].querySelector('img').getAttribute('src')+')';
+        }
+    }
+}
+ibg();*/
+
+$('.icon-menu').click(function(event){
+    $(this).toggleClass('active');
+    $('.menu__body').toggleClass('active');
+    $('.body').toggleClass('lock');
+});
+
+
+
+function ibg(){
+    $.each($('.ibg'), function(index, val){
+        if($(this).find('img').length > 0){
+            $(this).css('background-image','url("' + $(this).find('img').attr('src') + '")');
+        }
+    });
+};
+
+ibg();
